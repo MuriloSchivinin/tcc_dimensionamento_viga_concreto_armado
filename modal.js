886dd9eb0afe92539b2353d503add8c6cde8d496
Murilo Schivinin <murilo.skiva@gmail.com>
2021-10-08 21:33:54 -0300

    MANUTENÇÕES CAIXAS INFORMATIVAS E ADD SCROOLLBAR.CSS

--- modal.js
+++ modal.js
@@ -8,12 +8,120 @@ function processa_mensagem(id_mensagem) {
     switch (id_mensagem) {
         case 1:
             titulo = 'CLASSE DE RESISTÊNCIA DO CONCRETO (fck)';
-            texto = 'Teste descrição nº1';
+            texto = '-';
             break;
         case 2:
             titulo = 'CLASSE DE RESISTÊNCIA DO AÇO';
-            texto = 'numero 2 - teste de conteudo';
+            texto = '-';
             break;
+        case 3:
+            titulo = 'CLASSE DE AGRESSIVIDADE';
+            texto = '-';
+            break;
+        case 4:
+            titulo = 'VÃO EFETIVO (m)';
+            texto = '-';
+            break;
+        case 5:
+            titulo = 'LARGURA (CM)';
+            texto = '-';
+            break;
+        case 6:
+            titulo = 'DIÂMETRO ARMADURA TRANSVERSAL (Øt)';
+            texto = '-';
+            break;
+        case 7:
+            titulo = 'DIÂMETRO ARMADURA LONGITUDINAL (Øl)';
+            texto = '-';
+            break;
+        case 8:
+            titulo = 'COBRIMENTO mm';
+            texto = '-';
+            break;  
+        case 9:
+            titulo = 'ALTURA (PRE-DIMENSIONAMENTO) cm';
+            texto = '-';
+            break;
+        case 10:
+            titulo = 'ALTURA ÚTIL (d) cm';
+            texto = '-';
+            break;
+        case 11:
+            titulo = 'Mk kN.m';
+            texto = '-';
+            break;
+        case 12:
+            titulo = 'Md kN.m';
+            texto = '-';
+            break;
+        case 13:
+            titulo = 'Vsk kN.m';
+            texto = '-';
+            break;
+        case 14:
+            titulo = 'Vsd kN.m';
+            texto = '-';
+            break;
+        case 15:
+            titulo = 'As;min';
+            texto = '-';
+            break;
+        case 16:
+            titulo = 'As';
+            texto = '-';
+            break;
+        case 17:
+            titulo = `A's`;
+            texto = '-';
+            break;
+        case 18:
+            titulo = 'αv2';
+            texto = '-';
+            break;  
+        case 19:
+            titulo = 'Vrd2';
+            texto = '-';
+            break;
+        case 20:
+            titulo = 'fct';
+            texto = '-';
+            break;     
+        case 21:
+            titulo = 'Vc';
+            texto = '-';
+            break;
+        case 22:
+            titulo = 'fywk';
+            texto = '-';
+            break;
+        case 23:
+            titulo = 'ρsw;mín';
+            texto = '-';
+            break;
+        case 24:
+            titulo = 'Vsw,min';
+            texto = '-';
+            break;
+        case 25:
+            titulo = 'Vsd,min';
+            texto = '-';
+            break;
+        case 26:
+            titulo = 'asw,min';
+            texto = '-';
+            break;
+        case 27:
+            titulo = `Vd (kN)`;
+            texto = '-';
+            break;
+        case 28:
+            titulo = 'Vrd2 (kN)';
+            texto = '-';
+            break;  
+        case 29:
+            titulo = 'Vrd2 (kN)';
+            texto = '-';
+            break;                              
         default:
             titulo = '';
             texto = 'Descrição inválida!';
